c646a7085a7e63e4c7e9bf19b070b2ad6b426d7e
Davi Azevedo <47645089+daviaze@users.noreply.github.com>
2019-05-10 19:20:13 -0300

    atualização

--- APS II Desenv.Web/numero 1/javascript.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-let i = 0
-function receber (){
-while(i < Number(numero.value)){
-    postar.innerHTML += (`- ${2*i +2}<br>`)
-    i++
-}
-postar2.innerHTML = (`Soma: ${i + i*Number(numero.value)}`)
-}
--- functionexercícios/javascript2.js
+++
@@ -1,8 +0,0 @@
-function soma (array){
-    let somar = 0
-    array.forEach(numero => {
-                somar = somar + numero
-    });
-    return somar
-}
-console.log(soma([5,5,4]))
