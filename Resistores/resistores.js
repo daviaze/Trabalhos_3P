176239932cd1ac601b6dbe78e2c0943534573c0a
Davi Azevedo <47645089+daviaze@users.noreply.github.com>
2019-05-14 01:55:46 -0300

    ResistoresArray

--- Resistores/resistores.js
+++ Resistores/resistores.js
@@ -4,4 +4,3 @@ function resistor(cor) {
      for (let i = 0; i < cor.length;i++) numero = numero + String(array.indexOf(cor[i]))
      return (Number(numero))
  }
- console.log(resistor(["Marrom", "Amarelo"]))
